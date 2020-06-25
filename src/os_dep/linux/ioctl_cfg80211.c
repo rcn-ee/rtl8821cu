028b0d3fefa7cdbbd8559a3623c75043b8066712
Robert Nelson <robertcnelson@gmail.com>
2020-06-25 10:11:47 -0500

    cleanups
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- src/os_dep/linux/ioctl_cfg80211.c
+++ src/os_dep/linux/ioctl_cfg80211.c
@@ -334,8 +334,10 @@ static const struct ieee80211_txrx_stypes
 
 static u64 rtw_get_systime_us(void)
 {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 20 , 0))
-	return ktime_to_us(ktime_get_boottime());
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 20, 0))
+       struct timespec64 ts;
+       ktime_get_boottime_ts64(&ts);
+       return (ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
 	struct timespec ts;
 	get_monotonic_boottime(&ts);
