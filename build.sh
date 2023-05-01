43efc28b336724d6f561fe1dad3e4601b4e2375e
Robert Nelson <robertcnelson@gmail.com>
2023-05-01 15:47:14 -0500

    build.sh: fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ ARCH=$(uname -m)
 
 if [ -f .builddir ] ; then
 
-	if [ "x${ARCH}" = "xarmv7l" ] ; then
-		make_options="CROSS_COMPILE= KSRC=/build/buildd/linux-src"
-	else
+	if [ "x${ARCH}" = "xx86_64" ] ; then
 		x86_dir="`pwd`/../../normal"
 		if [ -f `pwd`/../../normal/.CC ] ; then
 			. `pwd`/../../normal/.CC
 			make_options="CROSS_COMPILE=${CC} KSRC=${x86_dir}/KERNEL"
 		fi
+	else
+		make_options="CROSS_COMPILE= KSRC=/build/buildd/linux-src"
 	fi
 
 	make ARCH=arm ${make_options} clean
